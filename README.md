8ef4c516f82a21412f8925b86a5bd5a25953ceed
alanbontoft <alan.bontoft@gmail.com>
2024-01-21 18:02:56 +0000

    Linux config added
    Example config files added for Linux.   Database name passed as parameter

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # QtConsoleSql
-Very simple console app to test access to MS SQL Server from Raspberry Pi
+Very simple console app to test access to MS SQL Server from Raspberry Pi & Linux
 
 install following on RPi:
 
@@ -14,3 +14,8 @@ copy following config files to /etc and /etc/freetds:
 odbc.ini
 odbcinst.ini
 freetds.conf
+
+Things I think are correct regarding ini/config files:
+1) An entry is required in odbcinst.ini for use by the connection string Driver parameter.  Only odbcinst.ini is required for database access via Qt code.
+2) When using the tsql utility, pass the section name (e.g. MySqlServer) from freetds.conf to the -S parameter, along with username and password
+3) It appears odbc.ini is only needed by the isql utility
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 
 #include <QtSql/QSqlDatabase>
 #include <QtSql/QSqlQuery>
+#include <QtSql/QSqlError>
 #include <QVariant>
 #include <iostream>
 
@@ -10,17 +11,37 @@ int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 
-    QString connStr = QStringLiteral("PROBEDB");
+    // check enough params passed, should be user, password, database name
+    if (argc < 4)
+    {
+        std::cout << QString("3 parameters expected, %1 passed").arg(argc - 1).toStdString() << std::endl;
+        return -1;
+    }
+
+    // create connection string
+    // Driver name must match an entry in /etc/odbcinst.ini
+    QString connStr;
+
+    // Microsoft driver
+    connStr = QStringLiteral("DRIVER={ODBC Driver 18 for SQL Server};SERVER=iot-server;PORT=1433;DATABASE=%1;TrustServerCertificate=yes;").arg(argv[3]);
+    // TDS driver
+    connStr = QStringLiteral("DRIVER={FreeTDS};SERVER=iot-server;PORT=1433;DATABASE=%1;TrustServerCertificate=yes;").arg(argv[3]);
+
+    // display connection string
+    std::cout << "Connection string: " << connStr.toStdString() << std::endl;
 
 
     QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
 
+    // apply connection string to database
     db.setDatabaseName(connStr);
 
     bool ok = false;
 
+    // open connection, passing user and password
     ok = db.open(argv[1], argv[2]);
 
+    // if connected, perfoem query on people table and display results
     if (ok)
     {
         QSqlQuery query;
@@ -37,8 +58,11 @@ int main(int argc, char *argv[])
             value = query.value("Age");
             std::cout << value.toInt() << std::endl;
         }
-
-
+    }
+    else
+    {
+        // display error message
+        std::cout << db.lastError().text().toStdString() << std::endl;
     }
 
     return a.exec();
